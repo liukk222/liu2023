427a999ffd8b285f48abd9eb25bdfac8f9e1e0cd
刘2022 <109359021+liukk222@users.noreply.github.com>
2023-02-12 00:29:52 +0800

    Add files via upload

---
+++ test_basicauth.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var secrets = gin.H{
+	"foo":    gin.H{"email": "foo@bar.com", "phone": "123433"},
+	"austin": gin.H{"email": "austin@example.com", "phone": "666"},
+	"lena":   gin.H{"email": "lena@guapa.com", "phone": "523443"},
+}
+
+func Handler(c *gin.Context) {
+	user := c.MustGet(gin.AuthUserKey).(string)
+	fmt.Println(user)
+	secret, ok := secrets[user]
+	if ok {
+		c.JSON(http.StatusOK, gin.H{"user": user, "secret": secret})
+	} else {
+		c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
+	}
+}
+
+func main() {
+	e := gin.Default()
+
+	e1 := e.Group("/admin", gin.BasicAuth(gin.Accounts{
+		"foo":    "bar",
+		"austin": "1234",
+		"lena":   "hello2",
+		"manu":   "4321",
+	}))
+	e1.GET("/secrets", Handler)
+	e.Run(":8888")
+}
