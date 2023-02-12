4b28965aae174ab744b0ef785fe3f55f28ee9ede
刘2022 <109359021+liukk222@users.noreply.github.com>
2023-02-13 00:34:51 +0800

    Add files via upload

---
+++ restful.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+type User struct {
+	UId  int    `json:"uid"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+var users = make([]User, 3)
+
+func init() {
+	u1 := User{1, "tom", 20}
+	u2 := User{2, "kite", 30}
+	u3 := User{3, "rose", 40}
+	users = append(users, u1)
+	users = append(users, u2)
+	users = append(users, u3)
+	fmt.Println(users)
+}
+func find(uid int) (*User, int) {
+	for i, u := range users {
+		if u.UId == uid {
+			return &u, i
+		}
+
+	}
+	return nil, -1
+}
+func (d *User) AddUser(c *gin.Context) {
+	users = append(users, *d)
+	c.JSON(200, users)
+}
+func Usersl(us User) *User {
+	return &us
+}
+
+func DelUser(c *gin.Context) {
+	uid := c.Param("uid")
+	id, _ := strconv.Atoi(uid)
+	_, i := find(id)
+	users = append(users[:i], users[i+1:]...)
+	c.JSON(200, users)
+}
+func UpdateUser(c *gin.Context) {
+	uid := c.Param("uid")
+	id, _ := strconv.Atoi(uid)
+	u, _ := find(id)
+	name := c.Param("name")
+
+	na := strconv.Quote(name) //nc := strconv.QuoteToASCII(name) 与na := strconv.Quote(name)一样
+	var nc string = na
+	nc = strings.Trim(na, `"`)
+	u.Name = nc
+	//u.Name = "修改的Name"
+	c.JSON(200, u)
+}
+func FindUser(c *gin.Context) {
+	uid := c.Param("uid")
+	id, _ := strconv.Atoi(uid)
+	u, _ := find(id)
+	c.JSON(200, u)
+}
+func main() {
+	e := gin.Default()
+	a := User{4, "liu", 20}
+	as := Usersl(a)
+	e.POST("/user/", as.AddUser)
+	e.PUT("/user/:uid/:name", UpdateUser)
+	e.GET("/user/:uid", FindUser)
+	e.DELETE("/user/:uid", DelUser)
+	e.Run(":8888")
+
+}
